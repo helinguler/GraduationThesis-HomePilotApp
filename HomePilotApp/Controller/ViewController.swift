6977bb6aabcbe9b92625f6398e0056b94c93c2c3
helinguler <gulerhelin@hotmail.com>
2024-12-14 17:04:17 +0300

    Home pilot app was created

---
+++ HomePilotApp/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomePilotApp
+//
+//  Created by Helin Güler on 11.12.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
