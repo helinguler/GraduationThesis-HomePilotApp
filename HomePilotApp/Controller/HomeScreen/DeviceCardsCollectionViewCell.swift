48707c3508675ae78cbd74a2149800d096a9875e
helinguler <gulerhelin@hotmail.com>
2024-12-19 23:13:35 +0300

    source cards was completed

---
+++ HomePilotApp/Controller/HomeScreen/DeviceCardsCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceCardsCollectionViewCell.swift
+//  HomePilotApp
+//
+//  Created by Helin Güler on 18.12.2024.
+//
+
+import UIKit
+
+class DeviceCardsCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var deviceNameLabel: UILabel!
+    @IBOutlet weak var deviceCostLabel: UILabel! 
+    @IBOutlet weak var deviceAddButton: UIButton!
+    
+    @IBOutlet weak var deviceCardsImage: UIImageView!
+}
--- HomePilotApp/Controller/HomeScreen/HomeViewController.swift
+++ HomePilotApp/Controller/HomeScreen/HomeViewController.swift
@@ -10,20 +10,32 @@ import UIKit
 class HomeViewController: UIViewController {
     
     @IBOutlet weak var sourceCardsCollectionView: UICollectionView!
+    @IBOutlet weak var deviceCardsCollectionView: UICollectionView!
     
+    // Source Cards Variables
     var energyTypeLabel = ["Electricity", "Water", "Natural Gas"]
     var sourceAmountLabel = ["0 kWH", "0 m³", "0 kwH"]
     var totalCostLabel = ["Total Cost: 0$", "Total Cost: 10$", "Total Cost: 20$"]
     var sourceCardsImages = ["1", "2", "3"]
     
-    // Adding timer
+    // Device Cards Variables
+    var deviceNameLabel = ["AC", "Washing Machine", "Natural Gas", "Water Despenser"]
+    var deviceCostLabel = ["Cost: 0$", "Cost: 10$", "Cost: 20$", "Cost: 30$"]
+    //var deviceAddButton = []
+    var deviceCardsImages = ["a", "b", "c", "d"]
+    
+    
+    // Adding timer to Source Cards
     var timer: Timer?
     var currentIndex = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        // Timer to Source Cards
         timer = Timer.scheduledTimer(timeInterval: 4.0, target: self, selector: #selector(moveToNext), userInfo: nil, repeats: true)
+        
     }
     
     // Moving Function for Timer
@@ -52,24 +64,67 @@ class HomeViewController: UIViewController {
 // Delegate and DataSource for SourceCardsCollectionView
 extension HomeViewController: UICalendarViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return sourceCardsImages.count
+        
+        if collectionView == sourceCardsCollectionView {
+            return sourceCardsImages.count
+        }
+        else if collectionView == deviceCardsCollectionView {
+            return deviceCardsImages.count
+        }
+        return 0
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = sourceCardsCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! SourceCardsCollectionViewCell
         
-        // Set the labels
-        cell.energyTypeLabel.text = energyTypeLabel[indexPath.row]
-        cell.sourceAmountLabel.text = sourceAmountLabel[indexPath.row]
-        cell.totalCostLabel.text = totalCostLabel[indexPath.row]
+        if collectionView == sourceCardsCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sourcecell", for: indexPath) as! SourceCardsCollectionViewCell
+                    
+            // Set SourceCardsCollectionView-specific data
+            cell.energyTypeLabel.text = energyTypeLabel[indexPath.row]
+            cell.sourceAmountLabel.text = sourceAmountLabel[indexPath.row]
+            cell.totalCostLabel.text = totalCostLabel[indexPath.row]
+            cell.sourceCardsImage.image = UIImage(named: sourceCardsImages[indexPath.row])
+                    
+            return cell
+            
+        }
+        else if collectionView == deviceCardsCollectionView {
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "devicecell", for: indexPath) as! DeviceCardsCollectionViewCell
+            
+            // Setting Border to devicecell
+            cell.layer.borderWidth = 1
+            cell.layer.borderColor = UIColor.gray.cgColor
+            cell.layer.cornerRadius = 25
+                
+            // Setting DeviceCardsCollectionView-specific data
+            cell.deviceNameLabel.text = deviceNameLabel[indexPath.row]
+            cell.deviceCostLabel.text = deviceCostLabel[indexPath.row]
+            cell.deviceCardsImage.image = UIImage(named: deviceCardsImages[indexPath.row])
+                    
+            return cell
+        }
         
-        // Set the image
-        cell.sourceCardsImage.image = UIImage(named: sourceCardsImages[indexPath.row])
-        
-        return cell
+        // Default return to avoid compilation issues
+        return UICollectionViewCell()
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: sourceCardsCollectionView.frame.width, height: sourceCardsCollectionView.frame.height)
+        
+        if collectionView == sourceCardsCollectionView {
+            return CGSize(width: sourceCardsCollectionView.frame.width, height: sourceCardsCollectionView.frame.height)
+        }
+        else if collectionView == deviceCardsCollectionView {
+            let spacing: CGFloat = 10
+            let totalSpacing = spacing * 3
+            let width = (collectionView.frame.width - totalSpacing) / 2
+            
+            return CGSize(width: width, height: width)
+            
+            //let size = (sourceCardsCollectionView.frame.width - 20) / 2
+            //return CGSize(width: size, height: size)
+        }
+         
+        return CGSize.zero
     }
 }
